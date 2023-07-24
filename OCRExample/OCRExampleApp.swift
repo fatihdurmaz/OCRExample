44b119a134ec2ce13d754ac2edd294aec99dea72
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-07-24 15:47:17 +0300

    Initial Commit

---
+++ OCRExample/OCRExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRExampleApp.swift
+//  OCRExample
+//
+//  Created by Fatih Durmaz on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct OCRExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
