f77bfbb1d271442fb8a1bb70b63aa5d9d4fb0866
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-08-01 10:51:52 +0300

    first commit

--- OCRExample/ContentView.swift
+++ OCRExample/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import ImageOCRUI
 
 struct ContentView: View {
+    @State private var texts: [ScannedText] = []
+    @State private var showScannerSheet = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                List(texts) { text in
+                    Text(text.scannedText)
+                }
+            }
+            .navigationTitle("OCR Image to Text")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Tara") {
+                        texts.removeAll()
+                        showScannerSheet = true
+                    }
+                    .sheet(isPresented: $showScannerSheet) {
+                        self.createScannerView()
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func createScannerView() -> ScannerView {
+        ScannerView(completion: { detectedTextPerPage in
+            if let concatenatedText = detectedTextPerPage?.joined(separator: "\n").trimmingCharacters(in: .whitespacesAndNewlines) {
+                let scannedData = ScannedText(scannedText: concatenatedText)
+                self.texts.append(scannedData)
+            }
+            self.showScannerSheet = false
+        })
     }
 }
 
